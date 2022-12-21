337624587a32c15c5aee19502a395dd81ac5cf56
kuhaha <kevin.yr.cheng@gmail.com>
2022-12-21 23:18:51 +0900

    Modified transformers

--- text_transformer.py
+++ text_transformer.py
@@ -2,16 +2,25 @@
 Transformers
 """
 
-def word_seq(text, weighted=False):
-    """" transforms text to a sequence of words """"
-    pass
+def word_seq(text, parser=None, weighted=False):
+    """ transforms text to a sequence of words """
+    
+    if parser is None:
+        return text.split()
+    
+    if type(parser) is str:
+        return text.split(sep=parser)
+   
+    return parser(text)
+    
 
 def word_bag(text, weighted=False):
-    """" transforms text to a bag (allowing duplicates) of words """"
+    """ transforms text to a bag (allowing duplicates) of words """
     pass
 
 def word_set(text, weighted=False):
-    """" transforms text to a set (removing duplicates) of words """"
+    """ transforms text to a set (removing duplicates) of words """
     bag = word_bag(text,weighted=weighted)
     return set(bag)
 
+    
