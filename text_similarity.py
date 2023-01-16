1e6b3b216576bab73f9095bfea7fa9888b209ce7
kuhaha <kevin.yr.cheng@gmail.com>
2023-01-16 23:50:30 +0900

    Add lcs support

--- text_similarity.py
+++ text_similarity.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 """
 Sequence Model
 """
@@ -39,3 +41,66 @@ def simpson_similarity(x, y):
     
     """
     return  simpson_coefficient(x, y)
+
+
+
+def lcs_seqlen(a, b):
+    """ Find length of the longest common sub-sequence between `a` , `b`
+    """
+    m, n = len(a), len(b)
+    c = np.zeros((m + 1, n + 1), dtype=int)
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if a[i-1] == b[j-1]:
+                c[i][j] = c[i-1][j-1] + 1
+            else:
+                c[i][j] = max(c[i][j-1], c[i-1][j])
+    return c[-1][-1]
+
+def lcs_seq(a, b):
+    s = ""
+    i, j = len(a), len(b)
+    while i > 0 and j > 0:
+        if a[i-1] == b[j-1]:
+            s = a[i-1] + s
+            i, j = i-1, j-1
+        elif c[i-1][j] >= c[i][j-1]:
+            i -= 1
+        else:
+            j -= 1
+    return s
+
+def lcs_strlen(a, b):
+     """ Finding length of longest common substring of a[0..m-1] and b[0..n-1]
+         Create a table to store lengths of longest common suffixes of substrings.
+         Note that LCSuff[i][j] contains the length of longest common suffix of
+         a[0...i-1] and a[0...j-1]. 
+    """
+    m, n = len(a), len(b)
+    
+    # `c` is the table with zero value initially in each cell    
+    c = np.zeros((m + 1, n + 1), dtype=int)
+    result = 0
+    # Following steps to build LCSuff[m+1][n+1] in bottom up fashion
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if (a[i-1] == b[j-1]):
+                c[i][j] = c[i-1][j-1] + 1
+                result = max(result, c[i][j])
+            else:
+                c[i][j] = 0
+    return result
+
+"""
+use difflib: builtin functions for comparing sequences
+cf.  https://docs.python.org/3/library/difflib.html
+"""
+from difflib import SequenceMatcher
+
+def lcs_str_size(a,b):
+    match = SequenceMatcher(None, a, b).find_longest_match()
+    return match.size
+
+def lcs_str(a,b):
+    match = SequenceMatcher(None, a, b, autojunk=False).find_longest_match()
+    return a[match.a:match.a+match.size]
